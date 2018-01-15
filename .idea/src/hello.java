4df24e59413b1fd7e643b80ea9b8bd669e64ad48
Xiao Mi <903674297@qq.com>
2018-01-15 09:47:21 +0800

    submit by xiaomigege at 2018.1.15 9:47

--- .idea/src/hello.java
+++ .idea/src/hello.java
@@ -1,5 +1,6 @@
 public class hello {
     public static void main(String[] args) {
         System.out.println("well come to my world");
+        System.out.println("well come to my world changed by xiaomi");
     }
 }
