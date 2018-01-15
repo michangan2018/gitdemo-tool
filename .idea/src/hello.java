0ac947c870a0bbd44e42eebcd06f6ec49803933e
Xiao Mi <903674297@qq.com>
2018-01-15 10:04:40 +0800

    submit by xiaomigege at 2018.1.15 9:52 @ dev branch

--- .idea/src/hello.java
+++ .idea/src/hello.java
@@ -3,6 +3,6 @@ public class hello {
         System.out.println("well come to my world");
         System.out.println("well come to my world changed by xiaomi");
         System.out.println("well come to my world changed by xiaomi:dev Branch");
-
+        System.out.println("well come to my world changed by xiaomi:dev1 Branch");
     }
 }
